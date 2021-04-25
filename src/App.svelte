e2f320317dd6dcbf44c6b476bc153e3fbbb7e741
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-25 15:43:52 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
 <script>
   import { onMount } from "svelte";
-  import {createRandomAvataar} from './routes/Avataars.svelte'
+  import {createRandomAvataar} from './routes/Avataars.js'
   export let date;
 /*  
   import _ from "underscore";
